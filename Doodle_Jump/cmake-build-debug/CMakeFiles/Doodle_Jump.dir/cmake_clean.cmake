6ed843f675abe172ffab42e53549b529c1b6a782
vald3nir <vald3nir@gmail.com>
2021-01-31 10:51:18 -0300

    update games

---
+++ Doodle_Jump/cmake-build-debug/CMakeFiles/Doodle_Jump.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Doodle_Jump.dir/main.cpp.o"
+  "Doodle_Jump"
+  "Doodle_Jump.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Doodle_Jump.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
