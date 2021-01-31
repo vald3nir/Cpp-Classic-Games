6a84ddb63e2fc595cf8c0280ebd9aaa7f07ef38e
vald3nir <vald3nir@gmail.com>
2021-01-31 10:59:50 -0300

    update games

---
+++ NetWalk/cmake-build-debug/CMakeFiles/NetWalk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetWalk.dir/main.cpp.o"
+  "NetWalk"
+  "NetWalk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NetWalk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
